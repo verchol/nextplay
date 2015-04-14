fc7684f88e171363be8787bc7206f4c7abc73f94
Oleg verhovsky <verchol@gmail.com>
2015-04-14 03:47:13 +0300

    fix

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -91,11 +91,11 @@ $(function() {
 
     $('.js-person-photo').attr('src', photo);
 
-    $('.js-person-back-btn')
+    $('.js-person-save-btn')
       .off('click', onClickPersonBackBtn)
       .on('click', onClickPersonBackBtn);
 
-    $('.js-person-save-btn')
+    $('.js-person-back-btn')
       .off('click', onClickPersonSaveBtn)
       .on('click', onClickPersonSaveBtn);
   }
