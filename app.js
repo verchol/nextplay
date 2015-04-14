ccef4af0bf1a3f09ac0e77fcfe20e440d91828eb
Oleg verhovsky <verchol@gmail.com>
2015-04-14 03:25:52 +0300

    fix

--- app.js
+++ app.js
@@ -16,7 +16,9 @@ function getDate(){
 
   return ret;
 }
+
 var app = express();
+
 app.enable('trust proxy');
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -49,9 +51,9 @@ onFileUploadComplete: function (file) {
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({extended: false, limit: '50mb'}));
+ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public', 'static')));
 app.use('/', routes);
