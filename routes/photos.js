ee80675439403bd1003721e9490f1b0df13f56ba
verchol <verchol@gmail.com>
2015-04-27 16:48:15 +0000

    support saving photo according to id

--- routes/photos.js
+++ routes/photos.js
@@ -4,12 +4,36 @@ var fs      = require('fs');
 var config  = require('../config');
 var router  = express.Router();
 
+
+
+function rename(req, res)
+{
+	var ip = req.headers['x-forwarded-for'] ||
+     req.connection.remoteAddress ||
+     req.socket.remoteAddress ||
+     req.connection.socket.remoteAddress;
+   var remoteIp = ip.split(':');
+
+   ip = remoteIp[remoteIp.length - 1];
+   var ips = ip.split('.');
+   ip = 'user_' + ips[ips.length - 1];
+
+   console.log('remote ip' + remoteIp[remoteIp.length - 1]);
+   console.log("ip:" + ip);
+   
+   
+   return ip;
+
+}
+
 router.post('/photo', function(req, res, next) {
   var photo = req.body.photo.replace(/^data:image\/png;base64,/, '');
-  var photoPath = path.resolve((config.photoLib || __dirname), './photos', Date.now().toString() + '.png');
+ 
 
   console.log('saving into :' + photoPath);
-
+  var ip  = rename(req, res);
+  //var photoPath = path.resolve((config.photoLib || __dirname), './photos', Date.now().toString() + '.png');
+  var photoPath = path.resolve((config.photoLib || __dirname), './photos', ip + '.png');
   fs.writeFile(photoPath, photo, 'base64', function(err) {
     if (!err) {
       console.log('photo posted');
