3c0f70e75d22a59c6caec76e815b6cbae26729f0
Aviad Blumenfeld <aviadblu@gmail.com>
2015-06-15 20:49:33 +0300

    fix bug

--- routes/imgman.js
+++ routes/imgman.js
@@ -1,90 +1,93 @@
 var express = require('express');
-var path    = require('path');
-var fs      = require('fs');
-var config  = require('../config');
-var router  = express.Router();
+var path = require('path');
+var fs = require('fs');
+var config = require('../config');
+var router = express.Router();
 var im = require('imagemagick');
 
 
-function rename(req, res)
-{
-	var ip = req.headers['x-forwarded-for'] ||
-     req.connection.remoteAddress ||
-     req.socket.remoteAddress ||
-     req.connection.socket.remoteAddress;
-   var remoteIp = ip.split(':');
+function rename(req, res) {
+    var ip = req.headers['x-forwarded-for'] ||
+        req.connection.remoteAddress ||
+        req.socket.remoteAddress ||
+        req.connection.socket.remoteAddress;
 
-   ip = remoteIp[remoteIp.length - 1];
-   var ips = ip.split('.');
-   ip = ips[ips.length - 1] + "";
-   ip = ip.substr(ip.length-2, 2);
-   
-   return ip;
+
+    var remoteIp = ip.split(':');
+
+
+    ip = remoteIp[remoteIp.length - 1];
+
+    var ips = ip.split('.');
+    ip = ips[ips.length - 1] + "";
+    ip = ip.substr(ip.length - 2, 2);
+
+
+    return ip;
 
 }
 
-router.get('/', function(req, res, next) {
-	
-	var img_name = req.query.img;
-	var photoPath = path.resolve((config.photoLib || __dirname), './photos', img_name);
-	
-	
-	im.identify(['-format', '%wx%h', photoPath], function(err, output){
-	  if (err) {
-	  	res.send(photoPath + ' || Error: ' + err);
-	  }
-	  else {
+router.get('/', function (req, res, next) {
 
-	  	res.send('dimension: '+output);
-	  }
-	});
-	
-	/*
-	im.readMetadata(photoPath, function(err, metadata){
-	  if (err) {
-	  	res.send(photoPath + ' || Error: ' + err);
-	  }
-	  else {
+    var img_name = req.query.img;
+    var photoPath = path.resolve((config.photoLib || __dirname), './photos', img_name);
 
-	  	res.send(metadata);
-	  }
-	});*/
-	//res.send("something");
+
+    im.identify(['-format', '%wx%h', photoPath], function (err, output) {
+        if (err) {
+            res.send(photoPath + ' || Error: ' + err);
+        }
+        else {
+
+            res.send('dimension: ' + output);
+        }
+    });
+
+    /*
+     im.readMetadata(photoPath, function(err, metadata){
+     if (err) {
+     res.send(photoPath + ' || Error: ' + err);
+     }
+     else {
+
+     res.send(metadata);
+     }
+     });*/
+    //res.send("something");
 });
 
-router.get('/clean', function(req, res, next) {
-	var ip  = rename(req, res);
-	var photoPath = path.resolve((config.photoLib || __dirname), './photos', ip + '.png');
-	
-	if (fs.existsSync(photoPath)) {
-		fs.unlink(photoPath);
-		res.send(photoPath + " deleted");
-	}
-	else {
-		res.send(photoPath + ' dose not exist');
-	}	
+router.get('/clean', function (req, res, next) {
+    var ip = rename(req, res);
+    var photoPath = path.resolve((config.photoLib || __dirname), './photos', ip + '.png');
+
+    if (fs.existsSync(photoPath)) {
+        fs.unlink(photoPath);
+        res.send(photoPath + " deleted");
+    }
+    else {
+        res.send(photoPath + ' dose not exist');
+    }
 
 });
 
-router.get('/reaize', function(req, res, next) {
-	var img_name = req.query.img;
-	var photoPath = path.resolve((config.photoLib || __dirname), './photos', img_name);
-	var dstPath = path.resolve((config.photoLib || __dirname), './photos', "test.png");
-	im.resize({
-	  srcPath: photoPath,
-	  dstPath: dstPath,
-	  width:   256
-	}, function(err, stdout, stderr){
-	  if (err) {
-	  	res.send(photoPath + ' || Error: ' + err);
-	  }
-	  else {
+router.get('/reaize', function (req, res, next) {
+    var img_name = req.query.img;
+    var photoPath = path.resolve((config.photoLib || __dirname), './photos', img_name);
+    var dstPath = path.resolve((config.photoLib || __dirname), './photos', "test.png");
+    im.resize({
+        srcPath: photoPath,
+        dstPath: dstPath,
+        width: 256
+    }, function (err, stdout, stderr) {
+        if (err) {
+            res.send(photoPath + ' || Error: ' + err);
+        }
+        else {
 
-	  	res.send('resized kittens.jpg to fit within 256x256px');
-	  }
-	});
+            res.send('resized kittens.jpg to fit within 256x256px');
+        }
+    });
 });
-
 
 
 module.exports = router;
--- routes/photos.js
+++ routes/photos.js
@@ -1,67 +1,64 @@
 var express = require('express');
-var path    = require('path');
-var fs      = require('fs');
-var config  = require('../config');
-var router  = express.Router();
+var path = require('path');
+var fs = require('fs');
+var config = require('../config');
+var router = express.Router();
 var im = require('imagemagick');
 
 
+function rename(req, res) {
+    var ip = req.headers['x-forwarded-for'] ||
+        req.connection.remoteAddress ||
+        req.socket.remoteAddress ||
+        req.connection.socket.remoteAddress;
 
-function rename(req, res)
-{
-	var ip = req.headers['x-forwarded-for'] ||
-     req.connection.remoteAddress ||
-     req.socket.remoteAddress ||
-     req.connection.socket.remoteAddress;
-   var remoteIp = ip.split(':');
 
-   ip = remoteIp[remoteIp.length - 1];
-   var ips = ip.split('.');
-   ip = ips[ips.length - 1];
+    var remoteIp = ip.split(':');
 
-   console.log('remote ip' + remoteIp[remoteIp.length - 1]);
-   console.log("ip:" + ip);
-   
-   
-   return ip;
+    ip = remoteIp[remoteIp.length - 1];
+
+    var ips = ip.split('.');
+    ip = ips[ips.length - 1] + "";
+    ip = ip.substr(ip.length - 2, 2);
+
+    return ip;
 
 }
 
-router.post('/photo', function(req, res, next) {
-  var photo = req.body.photo.replace(/^data:image\/png;base64,/, '');
-  
-  var ip  = rename(req, res);
-  //var photoPath = path.resolve((config.photoLib || __dirname), './photos', Date.now().toString() + '.png');
-  var photoPath = path.resolve((config.photoLib || __dirname), './photos', ip + '.png');
-  
-  console.log('saving into :' + photoPath);
-  
-  fs.writeFile(photoPath, photo, 'base64', function(err) {
-    if (!err) {
-    	
-    	// resize img
-    	im.resize({
-		  srcPath: photoPath,
-		  dstPath: photoPath,
-		  width: config.userImg.width,
-		  height: config.userImg.height,
-		}, function(err, stdout, stderr){
-		  if (err) {
-			  console.log('resize error...');
-		  	res.send(photoPath + ' || Error: ' + err);
-		  }
-		  else {
-	
-		  	res.send('ok');
-		  }
-		});
-		
-		
-      
-    } else {
-      console.log(err);
-    }
-  });
+router.post('/photo', function (req, res, next) {
+    var photo = req.body.photo.replace(/^data:image\/png;base64,/, '');
+
+    var ip = rename(req, res);
+    //var photoPath = path.resolve((config.photoLib || __dirname), './photos', Date.now().toString() + '.png');
+    var photoPath = path.resolve((config.photoLib || __dirname), './photos', ip + '.png');
+
+    console.log('saving into :' + photoPath);
+
+    fs.writeFile(photoPath, photo, 'base64', function (err) {
+        if (!err) {
+
+            // resize img
+            im.resize({
+                srcPath: photoPath,
+                dstPath: photoPath,
+                width: config.userImg.width,
+                height: config.userImg.height,
+            }, function (err, stdout, stderr) {
+                if (err) {
+                    console.log('resize error...');
+                    res.send(photoPath + ' || Error: ' + err);
+                }
+                else {
+
+                    res.send('ok');
+                }
+            });
+
+
+        } else {
+            console.log(err);
+        }
+    });
 });
 
 module.exports = router;
